45b2afb6ede5f9003406fdbd6726125e2f9a5f82
AbhishekPanta8 <72380871+AbhishekPanta8@users.noreply.github.com>
2021-05-29 17:33:43 +0530

    Add files via upload

---
+++ 2ndday/second.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string IncDecSt(string st){
+  int Charcount[26]={};
+  for(char ch: st){
+    Charcount[ch -'a']++;
+  }
+  string res ="";
+  int len = sizeof(Charcount)/sizeof(Charcount[0]);
+  int count = st.size();
+  while(count>0){
+    for(int i=0;i<26;i++){
+      if(Charcount[i]>0){
+        res.push_back(char(i+97));
+        Charcount[i]--;
+        count--;
+      }
+    }
+    for(int i=25;i>=0;i--){
+      if(Charcount[i]>0){
+        res.push_back(char(i+97));
+        Charcount[i]--;
+        count--;
+      }
+    }
+  }
+  return res;
+  cout<<len<<endl;
+}
+
+int main(){
+  string str={"aaaabbbbcccc"};
+  string st;
+  st = IncDecSt(str);
+  cout<<"string is:"<<endl;
+  cout<<st<<endl;
+  return 0;
+}
