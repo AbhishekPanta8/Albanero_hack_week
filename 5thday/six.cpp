ce47e7ca804ff959e8f00aa84ea09e4231811db5
AbhishekPanta8 <72380871+AbhishekPanta8@users.noreply.github.com>
2021-06-02 09:58:41 +0530

    Update six.cpp

--- 5thday/six.cpp
+++ 5thday/six.cpp
@@ -1,50 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-bool checkSentence(char str[])
-{
-    
-  int len = strlen(str);
+bool checkSentence(char str[]){
+  int len=strlen(str);
   if (str[0] < 'A' || str[0] > 'Z')
     return false;
   if (str[len - 1] != '.')
     return false;
-  int prev = 0, cur= 0;
-  int i = 1;
-  while (str[i])
-  {
-    if (str[i] >= 'A' && str[i] <= 'Z')
-      cur= 0;
-    else if (str[i] == ' ')
-      cur= 1;
-    else if (str[i] >= 'a' && str[i] <= 'z')
-      cur= 2;
-    else if (str[i] == '.')
-      cur= 3;
-    if (prev == cur && cur!= 2 )
+  for(int i=0;i<len;i++){
+    if(str[i]==' '&& str[i+1]==' ')
       return false;
-    if (prev == 2 && cur== 0)
-      return false;
-    if(prev == 1 && cur== 0)
-      return false;
-    if (cur== 3 && prev != 1)
-      return (str[i + 1] == '\0');
-    i++;
-    prev = cur;
   }
-  return false;
+  for(int i=1;i<len;i++)
+  {
+    if(((str[i] < 'a'||str[i] > 'z') && str[i]!=' '&& str[i]!='.' ))
+      return false;
+  }
+  return true;
 }
-
-int main()
-{
+int main(){
   bool x;
-  char *str[] = {"This sentence is syntactically correct.","This is Syntactically correct."};
+  char str[] = {"This sentence is syntactically correct."};
   int n = sizeof(str) / sizeof(str[0]);
   int i = 0;
-  for(int i=0;i<n;i++){
-    x=checkSentence(str[i]);
+  x=checkSentence(str);
   if(x)cout<<"true"<<endl;
   else cout<<"false"<<endl;
-  }
   return 0;
 }
